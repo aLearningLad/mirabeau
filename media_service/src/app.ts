cf648fd1177c24de3abdc4f24b1fe05eb7faef3d
aLearningLad <mocmanca@gmail.com>
2025-12-05 22:37:58 +0200

    jwt verification on express side, sorted

--- media_service/src/app.ts
+++ media_service/src/app.ts
@@ -1,9 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const { clerkMiddleware } = require("@clerk/express");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  clerkMiddleware({
+    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  })
+);
+
+console.log("Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+console.log("Secret Key:", process.env.CLERK_SECRET_KEY);
 
 module.exports = app;
--- media_service/src/index.ts
+++ media_service/src/index.ts
@@ -1,7 +1,25 @@
-const server = require("./app");
+import { Request, Response } from "express";
+const { getAuth } = require("@clerk/express");
+const app = require("./app");
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
+app.post("/api/v1/upload", async (req: Request, res: Response) => {
+  const auth = getAuth(req);
+  const token = req.headers.authorization?.split(" ")[1];
+
+  console.log("My token is here: ", token);
+
+  if (auth) {
+    console.log("auth here: ", auth);
+    return res.status(201).json({ message: "Image recieved, good sir!" });
+  }
+
+  return res
+    .status(401)
+    .json({ message: "User not authorized to perform this action" });
+});
+
+app.listen(PORT, () => {
   console.log(`Server is up & listening here: http://localhost:${PORT}`);
 });
